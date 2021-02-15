f873fc71b9fdebe64ee2ecfa929c2910bffbb760
root <aspire_8@163.com>
2021-02-15 22:36:26 +0800

    :sparkles:  hadoop hdfs Practice

---
+++ hadoop-hdfs/src/main/java/com/hdfs/practice/mkDir.java
@@ -0,0 +1,14 @@
+package com.hdfs.practice;
+
+import org.apache.hadoop.fs.FileSystem;
+
+/**
+ * @author Hale Lv
+ * @created 2021-02-15 22:23
+ * @project Github
+ */
+public class mkDir {
+    private static final String HDFS_PATH = "hdfs://39.106.208.58:8020";
+    private static final String HDFS_USER = "hdfs";
+    private static FileSystem fileSystem;
+}
