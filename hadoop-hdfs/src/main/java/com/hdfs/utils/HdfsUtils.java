94ffa38f38c77559c509db56a31630f9bb2b69bb
root <aspire_8@163.com>
2021-02-15 15:48:40 +0800

    :sparklet: hdfs utils FileSystem

---
+++ hadoop-hdfs/src/main/java/com/hdfs/utils/HdfsUtils.java
@@ -0,0 +1,15 @@
+package com.hdfs.utils;
+
+import org.apache.hadoop.fs.FileSystem;
+
+/**
+ * @author HaleLv
+ * @date 2021-02-15
+ **/
+public class HdfsUtils {
+    private static final String HDFS_PATH = "hdfs://39.106.208.58:8020";
+    private static final String HDFS_USER = "hdfs";
+    private static FileSystem fileSystem;
+
+
+}
