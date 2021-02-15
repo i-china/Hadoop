a762937d0c97da266dbc9382e81d730358cac401
root <aspire_8@163.com>
2021-02-15 22:42:09 +0800

    :sparkles:  hadoop hdfs Practice

--- hadoop-hdfs/src/main/java/com/hdfs/practice/mkDir.java
+++ hadoop-hdfs/src/main/java/com/hdfs/practice/mkDir.java
@@ -1,6 +1,12 @@
 package com.hdfs.practice;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author Hale Lv
@@ -11,4 +17,20 @@ public class mkDir {
     private static final String HDFS_PATH = "hdfs://39.106.208.58:8020";
     private static final String HDFS_USER = "hdfs";
     private static FileSystem fileSystem;
+
+    @Before
+    public void prepare() {
+        try{
+            Configuration configuration = new Configuration();
+            configuration.set("dfs.replication","3");
+            fileSystem = FileSystem.get(new URI(HDFS_PATH), configuration, HDFS_USER);
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }catch (URISyntaxException e){
+            e.printStackTrace();
+        }
+    }
+
 }
